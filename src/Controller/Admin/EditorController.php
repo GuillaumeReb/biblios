5505ad6b256e2a71ccd08400c005b9f730b392a4
Guillaume <guillaume.rebourgeon@hotmail.fr>
2024-12-02 16:54:17 +0100

    Formulaire livre en cours

---
+++ src/Controller/Admin/EditorController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/editor')]
+class EditorController extends AbstractController
+{
+    #[Route('', name: 'app_admin_editor')]
+    public function index(): Response
+    {
+        return $this->render('admin/editor/index.html.twig', [
+            'controller_name' => 'EditorController',
+        ]);
+    }
+}
