6ce1482d429456c8005cae4b3e05017dbc848126
Guillaume <guillaume.rebourgeon@hotmail.fr>
2025-01-06 14:51:07 +0100

    author est fini

--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -8,8 +8,10 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 class AuthorType extends AbstractType
 {
@@ -34,6 +36,13 @@ class AuthorType extends AbstractType
                 'choice_label' => 'id',
                 'multiple' => true,
                 'required' => false,
+            ])
+            ->add('certification', CheckboxType::class, [
+                'mapped' => false,
+                'label' => "Je certifie de l'exactitude des informations fournies.",
+                'constraints' => [
+                    new IsTrue(message: "Vous devez cochez la case pour ajouter un auteur.")
+                ],
             ])
         ;
     }
