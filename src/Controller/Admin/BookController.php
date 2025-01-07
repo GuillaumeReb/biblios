f7b6ce8863774689bf6e85c6731903c9d91d2e81
Guillaume <guillaume.rebourgeon@hotmail.fr>
2025-01-07 18:12:28 +0100

    Début de show pour book

--- src/Controller/Admin/BookController.php
+++ src/Controller/Admin/BookController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Book;
 use App\Form\BookType;
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,11 +14,14 @@ use Doctrine\ORM\EntityManagerInterface;
 #[Route('/admin/book')]
 class BookController extends AbstractController
 {
-    #[Route('', name: 'app_admin_book_index')]
-    public function index(): Response
+    #[Route('', name: 'app_admin_book_index', methods: ['GET'])]
+    public function index(BookRepository $repository): Response
     {
+        $books = $repository->findAll();
+
         return $this->render('admin/book/index.html.twig', [
             'controller_name' => 'BookController',
+            'books' => $books,
         ]);
     }
 
@@ -37,6 +41,14 @@ class BookController extends AbstractController
         
         return $this->render('admin/book/new.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_book_show', requirements: ['id' => '\d+'],methods: ['GET'])]
+    public function show(?Book $book): Response
+    {
+        return $this->render('admin/book/show.html.twig', [
+            'book' => $book,
         ]);
     }
 
